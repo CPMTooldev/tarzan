d6a968be4e68d30eb86a7f115a278490548a541c
CPMTooldev <telmnbattr@gmail.com>
2025-04-14 23:40:02 +0800

    Update cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://garden.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://tanzanshop.squareweb.app/api"
 
 class CPMTooldev:
     def __init__(self, access_key) -> None:
